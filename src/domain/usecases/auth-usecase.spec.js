bf7f1e3f9a96cef8a0200c9fe08650962c6ff36f
Adriano Santos <adrianodrix@gmail.com>
2019-12-26 17:44:20 -0300

    ensure AuthUseCase throw if no email is provided

---
+++ src/domain/usecases/auth-usecase.spec.js
@@ -0,0 +1,12 @@
+class AuthUseCase {
+  async auth (email) {
+    if (!email) throw new Error()
+  }
+}
+describe('Auth UseCase', () => {
+  test('should throw if no email is provided', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth()
+    await expect(promise).rejects.toThrow()
+  })
+})
