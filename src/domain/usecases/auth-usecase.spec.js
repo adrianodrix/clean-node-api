88c7bc2a2e6c56f00d30ddeff5c25ca4a4cbc7a7
Adriano Santos <adrianodrix@gmail.com>
2019-12-26 18:15:22 -0300

    ensure AuthUseCase returns null if LoadUserByEmailRepository returns null

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -15,7 +15,10 @@ class AuthUseCase {
       throw new InvalidParamError('loadUserByEmailRepository')
     }
 
-    await this.loadUserByEmailRepository.load(email)
+    const user = await this.loadUserByEmailRepository.load(email)
+    if (!user) {
+      return null
+    }
   }
 }
 
@@ -68,5 +71,11 @@ describe('Auth UseCase', () => {
     await expect(promise).rejects.toThrow(
       new InvalidParamError('loadUserByEmailRepository')
     )
+  })
+
+  test('should return null if LoadUserByEmailRepository returns null', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.auth('invalid_email@mail.com', 'any_password')
+    expect(accessToken).toBeNull()
   })
 })
