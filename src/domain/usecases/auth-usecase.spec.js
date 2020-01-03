36e1ebe859d8c402d7d90174eeafb1ccacfe82dd
Adriano Santos <adrianodrix@gmail.com>
2020-01-03 09:52:42 -0300

    remove unnecessaries ifs

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,4 +1,4 @@
-const { MissingParamError, InvalidParamError } = require('../../utils/errors')
+const { MissingParamError } = require('../../utils/errors')
 const AuthUseCase = require('./auth-usecase')
 
 const makeSut = () => {
@@ -39,17 +39,13 @@ describe('Auth UseCase', () => {
   test('should trhow if no LoadUserByEmailRepository is provided', async () => {
     const sut = new AuthUseCase()
     const promise = sut.auth('any_email@mail.com', 'any_password')
-    await expect(promise).rejects.toThrow(
-      new MissingParamError('loadUserByEmailRepository')
-    )
+    await expect(promise).rejects.toThrow()
   })
 
   test('should trhow if no LoadUserByEmailRepository has no load method', async () => {
     const sut = new AuthUseCase({})
     const promise = sut.auth('any_email@mail.com', 'any_password')
-    await expect(promise).rejects.toThrow(
-      new InvalidParamError('loadUserByEmailRepository')
-    )
+    await expect(promise).rejects.toThrow()
   })
 
   test('should return null if LoadUserByEmailRepository returns null', async () => {
