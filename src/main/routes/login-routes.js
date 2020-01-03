67aadded2a527828373e7727dc1dc8bb197e8cd0
Adriano Santos <adrianodrix@gmail.com>
2020-01-03 18:18:18 -0300

    improve loginRouterComposer

--- src/main/routes/login-routes.js
+++ src/main/routes/login-routes.js
@@ -1,6 +1,6 @@
-const loginRouter = require('../composers/login-router-composer')
-const RouterAdapter = require('../adapters/express-router-adapter')
+const LoginRouterComposer = require('../composers/login-router-composer')
+const { adapt } = require('../adapters/express-router-adapter')
 
 module.exports = async router => {
-  router.post('/login', RouterAdapter.adapt(loginRouter))
+  router.post('/login', adapt(LoginRouterComposer.compose()))
 }
