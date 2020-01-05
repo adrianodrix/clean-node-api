3d7fbf8d70f8fbcab15d294492a37564e94fcc52
Adriano Santos <adrianodrix@gmail.com>
2020-01-05 10:58:48 -0300

    add user admin when server started

--- src/main/index.js
+++ src/main/index.js
@@ -1,8 +1,20 @@
 const MongoHelper = require('../infra/helpers/mongo-helper')
+const bcrypt = require('bcrypt')
 const env = require('./config/env')
 
 MongoHelper.connect(env.mongoUrl)
-  .then(() => {
+  .then(async () => {
+    const userModel = await MongoHelper.getCollection('users')
+    const user = await userModel.findOne({
+      email: 'adrianodrix@gmail.com'
+    })
+    if (!user) {
+      await userModel.insertOne({
+        email: 'adrianodrix@gmail.com',
+        password: bcrypt.hashSync('123456', 10)
+      })
+    }
+
     const app = require('./config/app')
     app.listen(env.port, () =>
       console.log(`Server running at http://localhost:${env.port}`)
